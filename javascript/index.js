dd02c1ece5bd7162b89c1e19a18128b284cf7c6a
Brice THIBAULT <70805751+Darkjulius@users.noreply.github.com>
2023-05-05 08:35:15 +0200

    css /javascript
    Fin  de saisi du CSS en vue mobile. Il reste les media queries et encore d'autres tâches à effectuer.
    
    JavaScript: Déclaration des constantes.

---
+++ javascript/index.js
@@ -0,0 +1,5 @@
+const form = document.getElementById("form");
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
