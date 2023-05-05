b097e573089b4769ec948d2c6dddd595bc35dc1b
Brice THIBAULT <70805751+Darkjulius@users.noreply.github.com>
2023-05-05 09:36:55 +0200

    Fin JavaScript
    JavaScript avec tests: Ok.

--- javascript/index.js
+++ javascript/index.js
@@ -3,3 +3,70 @@ const firstname = document.getElementById("firstname");
 const lastname = document.getElementById("lastname");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
+
+// console.log(form, firstname, lastname, email, password);
+
+/**
+ * La fonction checkInputs va contrôler tous les inputs avant envoi du formulaire.
+ */
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputs();
+});
+
+function checkInputs() {
+  // Récupération des valeurs saisies dans les inputs et je supprime les espaces s'il y en a avec la méthode trim().
+  const firstnameValue = firstname.value.trim();
+  const lastnameValue = lastname.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  // console.log(firstnameValue, lastnameValue, emailValue, passwordValue);
+
+  if (firstnameValue === "") {
+    setErrorFor(firstname, "First Name cannot be empty");
+  } else {
+    setSuccessFor(firstname);
+  }
+
+  if (lastnameValue === "") {
+    setErrorFor(lastname, "Last Name cannot be empty");
+  } else {
+    setSuccessFor(lastname);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "Email cannot be empty");
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "Looks like this is not an email");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === "") {
+    setErrorFor(password, "Password cannot be empty");
+  } else {
+    setSuccessFor(password);
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  // Ajout de la class error
+  formControl.className = "form-control error";
+
+  // Ajout du message d'erreur à l'intérieur de l'élément HTML <small></small>
+  small.innerText = message;
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+function isEmail(email) {
+  return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
